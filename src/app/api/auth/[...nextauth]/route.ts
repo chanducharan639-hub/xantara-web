8fd290d00d68058b6e016c6f63e0f267649fdd6b
chanducharan639-hub <chanducharan639@gmail.com>
2026-05-28 14:51:04 +0530

    Fix NextAuth callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,12 +8,7 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
-
     secret: process.env.NEXTAUTH_SECRET,
-
-    pages: {
-        signIn: "/login",
-    },
 });
 
 export { handler as GET, handler as POST };
